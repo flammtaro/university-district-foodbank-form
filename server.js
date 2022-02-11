2bdaa3e3d106e781e2d275c598a248d0bae045c7
Denise Arevalo <denisearev@gmail.com>
2022-02-11 11:44:41 -0800

    sign up and server changes

--- server.js
+++ server.js
@@ -36,6 +36,42 @@ app.use(session({
 
 app.use('/',allRoutes);
 
+
+// // nodemailer
+// // let transporter = nodemailer.createTransport(transport[ defaults])
+// // transporter.sendMail(data[ callback])
+// app.post("/", async (req, res) => {
+//     const {email} = req.body;
+//     let transporter = nodemailer.createTransport({
+//         host: "smtp.ethereal.email",
+//         port: 587,
+//         secure: false, // true for 465, false for other ports
+//         auth: {
+//           user: testAccount.user, // generated ethereal user
+//           pass: testAccount.pass, // generated ethereal password
+//         },
+//       });
+    
+//       // send mail with defined transport object
+//     const msg = {
+//         from: 'need a tester email', // sender address
+//         to: `${email}`, // list of receivers
+//         subject: "Reset password", // Subject line
+//         text: "Please select the link to reset the password", // plain text body
+//         html: "", // html body
+//     }
+//     const info = await transporter.sendMail(msg);
+      
+    
+//       console.log("Message sent: %s", info.messageId);
+//       // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    
+//       // Preview only available when sending through an Ethereal account
+//       console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+//       // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+//       res.send('Email Sent!')
+// });
+
 sequelize.sync({ force: false }).then(function() {
     app.listen(PORT, function() {
     console.log('App listening on PORT ' + PORT);
