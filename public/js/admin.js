37e636f07ab919a30312e0559b3e8ae9e1af3f03
Joe Rehfuss <joe.rehfuss@gmail.com>
2022-05-12 12:25:01 -0700

    adding print screen, estimated wait times, new options for restrictions, and feedback form

--- public/js/admin.js
+++ public/js/admin.js
@@ -17,27 +17,40 @@ deleteBtns.forEach((button) => {
   });
 });
 
-const specialsInput = document.querySelector(".form-control");
-const specialsBtn = document.querySelector("#specialsBtn");
+const printBtns = document.querySelectorAll(".print-button");
+const orderCards = document.querySelectorAll(".order-card");
+printBtns.forEach(btn=>{
+  btn.addEventListener("click",e=>{
+    const card = e.target.parentElement.parentElement;
+    orderCards.forEach(oc=>{
+      oc.classList.remove("print-selected")
+    })
+    card.classList.add("print-selected")
+    window.print()
+  })
+})
+
+// const specialsInput = document.querySelector(".form-control");
+// const specialsBtn = document.querySelector("#specialsBtn");
 
 
 
-specialsBtn.addEventListener("click", (es) => {
-  console.log(specialsInput.value);
-  es.preventDefault();
-  let input = { specialInput: specialsInput.value };
-  fetch("/api/specials", {
-    method: "POST",
-    body: JSON.stringify(input),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((res) => {
+// specialsBtn.addEventListener("click", (es) => {
+//   console.log(specialsInput.value);
+//   es.preventDefault();
+//   let input = { specialInput: specialsInput.value };
+//   fetch("/api/specials", {
+//     method: "POST",
+//     body: JSON.stringify(input),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   }).then((res) => {
    
-    if (res.ok) {
-      location.reload();
-    } else {
-      console.log("Failed to delete.");
-    }
-  });
-});
+//     if (res.ok) {
+//       location.reload();
+//     } else {
+//       console.log("Failed to delete.");
+//     }
+//   });
+// });
